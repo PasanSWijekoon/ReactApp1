43351760392b95ee7557c331d05fb915a60debd1
Pasanwijekoon <pasanwijekoon673@gmail.com>
2023-01-19 22:43:18 +0530

    First project git

--- App.tsx
+++ App.tsx
@@ -76,18 +76,17 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="My First Project">
+            This Is My FIrst Project
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <Section title="How You Feel">
+            I am Feling happy
           </Section>
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
+          <Section title="Your Details">
+           I am feeling happy
           </Section>
           <LearnMoreLinks />
         </View>
